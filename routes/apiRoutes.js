c1e18cf822b5ebc233984d8bb5c05d4e57c401ed
levinemr2 <levinemr2@vcu.edu>
2019-05-21 19:41:55 -0400

    added bug count table and stream table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,23 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/bugTable", function(req, res) {
+    db.Example.findAll({}).then(function(dbBugTables) {
+      res.json(dbBugTables);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/bugTable", function(req, res) {
+    db.bugTable.create(req.body).then(function(dbBugTable) {
+      res.json(dbBugTable);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/bugTable/:id", function(req, res) {
+    db.BugTable.destroy({ where: { id: req.params.id } }).then(function(dbBugTable) {
+      res.json(dbBugTable);
     });
   });
 };
