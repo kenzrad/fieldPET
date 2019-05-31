a9684a2ddfc61fe9e4010799983748a49a11975e
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-05-31 00:21:35 -0400

    added date and site info collection to login modal

--- routes/sites-api-routes.js
+++ routes/sites-api-routes.js
@@ -3,9 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all bug data associated with a site
   app.get("/api/Site", function(req, res) {
-    db.Site.findAll({
-      include: [db.Bugs]
-    }).then(function(dbSite) {
+    db.Site.findAll().then(function(dbSite) {
       console.log(dbSite);
       res.json(dbSite);
     });
