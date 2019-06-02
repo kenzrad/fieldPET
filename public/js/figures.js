de343bc0bfc81ffb5c14c12ea3226899db4ba287
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-06-02 14:47:10 -0400

    updates to metrics table and some general cleanup

--- public/js/figures.js
+++ public/js/figures.js
@@ -2,10 +2,6 @@ var ctx = $('#chart-display');
 
 
 //For now, we have temporary buttons to run various functions. Later we should allow more flexibility with drop-down menus and other options so the user can decide what to do (but this will be a quick way to get this going)
-$("#test-chart-btn").on("click", function() {
-    testChart();
-    console.log("testing this chart shit")
-});
 
 $("#get-all-btn").on("click", function() {
     window.location = 'https://salty-savannah-46210.herokuapp.com/api/bugs'
@@ -20,8 +16,8 @@ $("#all-SF-btn").on("click", function() {
     console.log("Getting Stonefly Data")
 });
 
-$("#lj-condition-btn").on("click", function() {
-    getLjCondition();
+$("#site-condition-btn").on("click", function() {
+    getSiteCondition();
     
 });
 
@@ -34,31 +30,6 @@ $("#lower-james-MF-btn").on("click", function() {
     console.log("Getting Mayfly Data");
     
 });
-
-
-function testChart() {
-    var chartDisplay = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45]
-            }]
-        },
-
-        // Configuration options go here
-        options: {}
-    });
-
-    $("#chart-title").text("Test Chart");
-    showChart();
-};
 
 function getSfData(data) {
     var item = data.map(function(item){
@@ -85,14 +56,12 @@ function getSfData(data) {
         
             }]
         },
-options: {}
-
+        options: {}
     });
 
     $('#chart-title').text('Stone Fly Chart');
     showChart();
    
-    console.log("This function will show us all the stonefly data, either organized by site or over time with sites color coded, who knows");
 };
 
 function getLjMf(data) {
@@ -128,7 +97,7 @@ function getLjMf(data) {
     console.log("This function will show us the number of mayflies in the james river over time");
 };
 
-function getLjCondition() {
+function getSiteCondition() {
     console.log("This function will show us the condition score of the james river over time -- this is a particularily complicated function, so we may change it quite a bit");
 };
 
